8662e394ae7059a35c73e9781bd256c06baad01d
David Vergaray <david.vergaray.a@gmail.com>
2022-04-04 13:43:39 -0400

    Add teacher class

---
+++ teacher_class.rb
@@ -0,0 +1,15 @@
+require './person_class'
+
+# Teacher Class
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
