6ae375d33b605436bc912d7cc06003cfc1966c55
David Vergaray <david.vergaray.a@gmail.com>
2022-04-05 19:52:07 -0400

    Add Classroom class

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+# Classroom class
+class Classroom
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
+end
