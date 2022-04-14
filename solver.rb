7304f2b1e9f2dfa8b0115edbd157b1d4c8194114
David Vergaray <david.vergaray.a@gmail.com>
2022-04-14 17:29:02 -0400

    Resolve all 4 fizz buzz tests

--- solver.rb
+++ solver.rb
@@ -16,8 +16,8 @@ class Solver
   end
 
   def fizzbuzz(num)
-    return 'fizz' if num == 3
-    return 'buzz' if num == 5
     return 'fizzbuzz' if num == 15
+    return 'fizz' if num % 3 == 0
+    return 'buzz' if num == 5
   end
 end
